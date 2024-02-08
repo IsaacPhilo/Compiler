f1e4ca24cdf619448136b423a712a802ab84a0d6
IsaacP <isaacphilo1@gmail.com>
2024-02-07 23:54:57 -0600

    Added more description to the identifier technicality

--- src/main/java/ASTIdentifier.java
+++ src/main/java/ASTIdentifier.java
@@ -1,5 +1,5 @@
 public class ASTIdentifier extends ASTNode{
-    public static final String regularExpression = "[a-zA-Z][a-zA-Z0-9]*";//This will match all alphabetic keywords; since this is the case, client tokenizer code will need to ignore this class as an option when considering which node type to create
+    public static final String regularExpression = "[a-zA-Z][a-zA-Z0-9]*";//This will match all alphabetic keywords; since this is the case, client tokenizer code will need to ignore this class as an option when considering which node type to create when the analyzed string, having whitespace on the right and (whitespace or token boundaries) on the left, matches an alphabetic keyword
     private String type;
     public ASTIdentifier(String variableType, String name) {
         super(name);
